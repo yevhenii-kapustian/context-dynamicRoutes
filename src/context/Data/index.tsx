17e69f5ff0956e9ebc2801c753a86d6e0df2a7c2
Yevhenii <ke1vin.kapustian@gmail.com>
2025-08-31 17:10:08 +0200

    Made context

---
+++ src/context/Data/index.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { createContext, useContext } from "react"
+
+const DataContext = createContext<DataType | null>(null)
+
+type FetchDataType = {
+    url: string
+}
+
+type DataType = {
+    fetchData: (url: FetchDataType) => void
+}
+
+export const Data = ({children}: {children: React.ReactNode}) => {
+
+    const fetchData = async ({url}:FetchDataType) => {
+        try {
+            const response = await fetch(url)
+            return await response.json()
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return(
+        <DataContext.Provider value={{fetchData}}>
+            {children}
+        </DataContext.Provider>
+    )
+}
+
+export const useDataContext = () => {
+    return useContext(DataContext)
+}

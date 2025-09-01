0958ce6e9ec4094556e6858f73dbeb4ba3b4ff4f
Yevhenii <ke1vin.kapustian@gmail.com>
2025-09-01 11:34:19 +0200

    removed nav teg

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -3,11 +3,7 @@ import { navigationLinks } from "@/data/navigation"
 import { NavigationType } from "@/types/navigationTypes"
 
 const Navigation = () => {
-    return(
-        <nav>
-            {navigationLinks.map((item:NavigationType, index: number) => <Link key={index} href={item.link}>{item.name}</Link> )}
-        </nav>
-    )
+    return <>{navigationLinks.map((item:NavigationType, index: number) => <Link key={index} href={item.link}>{item.name}</Link> )}</>
 }
 
 export default Navigation

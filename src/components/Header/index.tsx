fdf858bc582ab567f3bd6714edb3b385877efe05
Yevhenii <ke1vin.kapustian@gmail.com>
2025-09-01 11:32:49 +0200

    ,

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,13 @@
-import UserName from "../UserName"
 import Navigation from "../Navigation"
 import Logo from "../Logo"
 
 const Header = () => {
     return(
-        <header>
+        <header className="flex justify-evenly items-center text-[#2C2D35] bg-[#E1EDE6]">
             <Logo/>
-            <UserName/>
-            <Navigation/>
+            <div className="flex gap-5">
+                <Navigation/>
+            </div>
         </header>
     )
 }

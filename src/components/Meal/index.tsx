09ea8e7722bc4970f183b40b8b71d94618917119
Yevhenii <ke1vin.kapustian@gmail.com>
2025-09-10 13:42:45 +0200

    added button

--- src/components/Meal/index.tsx
+++ src/components/Meal/index.tsx
@@ -6,17 +6,20 @@ import Image from "next/image"
 import Link from "next/link"
 
 const Meal = () => {
-    const {savedUserMeal, setSavedUserMealId} = useMealContext() as MealValueType
+    const {savedUserMeal, setSavedUserMealId, removeMeal} = useMealContext() as MealValueType
     const handleSaveId = (id: string) => {
         setSavedUserMealId(id)
     }
     return(
         <div>
             {savedUserMeal.map(item => (
+                <>
                 <Link href={`/categories/${item.strMeal}`} onClick={() => handleSaveId(item.idMeal as string)} key={item.idMeal}>
                     <Image src={item.strMealThumb} alt={item.strMeal} width={300} height={300}/>
                     <h5>{item.strMeal}</h5>
                 </Link>
+                <button onClick={() => removeMeal(item.idMeal)}>Remove</button>
+                </>
             ))}
         </div>
     )
--- src/types/meal.ts
+++ src/types/meal.ts
@@ -5,7 +5,7 @@ export type MealType = {
     strMeal: string,
     strMealThumb: string
     strArea: string,
-    idMeal?: string,
+    idMeal: string,
     strCategory?: string,
     strInstructions?: string,
 } & {

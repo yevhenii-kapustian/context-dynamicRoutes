8868ef30cff064b7f382985565374dd5d2056ebc
Yevhenii <ke1vin.kapustian@gmail.com>
2025-09-14 23:41:08 +0200

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Montserrat, Roboto } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
